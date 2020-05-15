ec64fe09dbf993237aa79f4956e4ce8516b15562
Liliia <46443871+lilavera@users.noreply.github.com>
2020-05-15 03:47:19 +0200

    removing searching by shiftStart and shiftEnd

--- src/main/java/com/verkhogliadoval/repository/TimetableRepository.java
+++ src/main/java/com/verkhogliadoval/repository/TimetableRepository.java
@@ -10,7 +10,7 @@ import java.util.List;
 
 public interface TimetableRepository extends JpaRepository<TimeTable,Long> {
     List<TimeTable> findAllByDayOfWeekAndShiftStartAndShiftEndAndDoctor(int day, LocalTime shiftStart, LocalTime shiftEnd, Doctor doctor);
-    TimeTable findAllByShiftStartAndDoctor(LocalTime shiftStart, Doctor doctor);
-    TimeTable findAllByShiftEnd(LocalTime shiftEnd,Doctor doctor);
+    //TimeTable findAllByShiftStartAndDoctor(LocalTime shiftStart, Doctor doctor);
+   // TimeTable findAllByShiftEnd(LocalTime shiftEnd,Doctor doctor);
     TimeTable findAllByDoctor(Doctor doctor);
 }
